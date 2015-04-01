dd4580cd5e24f015b2cb60e507a4e4bf8d767282
Egor Chesakov <echesako@uwo.ca>
2015-04-01 18:31:30 -0400

    Knn search prototype was implemented.

---
+++ LevenbergMarquardtMinimizer/TestKnnSearch.h
@@ -0,0 +1,8 @@
+#ifndef TestKnnSearch_h
+#define TestKnnSearch_h
+
+#include <vector>
+
+void testKnnSearch(float* pP, float *pSigma, float* pS, float* pT, std::vector<int>& indPi, std::vector<int>& indPj, int numPoints);
+
+#endif//TestKnnSearch_h
