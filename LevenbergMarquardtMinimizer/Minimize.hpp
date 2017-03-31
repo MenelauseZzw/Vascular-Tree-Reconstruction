dae46152549c9c3dd69d7aca9ebf4437b84a4f4e
Egor Chesakov <echesako@uwo.ca>
2017-03-31 11:06:19 -0400

    Remove Minimize.cu and Minimize.hpp

--- LevenbergMarquardtMinimizer/Minimize.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef Minimize_hpp
-#define Minimize_hpp
-
-//void Minimize(float* pTildeP, float* pS, float* pT, float* pSigma, int numPoints, int* pIndPi, int* pIndPj, int numPairs, float* pP, int maxIterations);
-
-template<typename ValueType, typename IndexType>
-void Minimize(const ValueType* pMeasurements, ValueType* pTangentLinesPoints1, ValueType* pTangentLinesPoints2, const ValueType* pRadiuses, int numMeasurements, const IndexType* pIndices1, const IndexType* pIndices2, int indicesLength, ValueType* pPositions, int itnlim, ValueType* pDistances, ValueType* pCurvatures);
-
-#endif//Minimize_hpp
