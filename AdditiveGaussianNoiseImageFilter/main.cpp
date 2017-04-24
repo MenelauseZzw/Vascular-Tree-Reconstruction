08ff2d8394edb29eb28093b087c07fdfbbd77f73
Egor Chesakov <echesako@uwo.ca>
2017-04-24 18:19:52 -0400

    Add PSNR to AdditiveGaussianNoiseImageFilter

--- AdditiveGaussianNoiseImageFilter/main.cpp
+++ AdditiveGaussianNoiseImageFilter/main.cpp
@@ -1,5 +1,10 @@
 #include <boost/program_options.hpp>
+#include <boost/log/trivial.hpp>
 #include <itkAdditiveGaussianNoiseImageFilter.h>
+#include <itkMetaDataDictionary.h>
+#include <itkMetaDataObject.h>
+#include <itkSquaredDifferenceImageFilter.h>
+#include <itkStatisticsImageFilter.h>
 #include <itkImage.h>
 #include <itkImageFileReader.h>
 #include <itkImageFileWriter.h>
@@ -10,14 +15,22 @@ void DoAdditiveGaussianNoiseImageFilter(
   const std::string& inputFileName,
   const std::string& outputFileName, double mean, double standardDeviation)
 {
-  typedef short ValueType;
+  typedef double ValueType;
   constexpr unsigned int NumDimensions = 3;
+
+  BOOST_LOG_TRIVIAL(info) << "input filename = \"" << inputFileName << "\"";
+  BOOST_LOG_TRIVIAL(info) << "output filename = \"" << outputFileName << "\"";
+
+  BOOST_LOG_TRIVIAL(info) << "mean = " << mean;
+  BOOST_LOG_TRIVIAL(info) << "standardDeviation = " << standardDeviation;
 
   typedef itk::Image<ValueType, NumDimensions> ImageType;
   typedef itk::Index<NumDimensions> IndexType;
   typedef typename ImageType::SizeType SizeType;
 
   typedef itk::AdditiveGaussianNoiseImageFilter<ImageType, ImageType> AdditiveGaussianNoiseImageFilterType;
+  typedef itk::SquaredDifferenceImageFilter<ImageType, ImageType, ImageType> SquaredDifferenceImageFilterType;
+  typedef itk::StatisticsImageFilter<ImageType> StatisticsImageFilterType;
   typedef itk::ImageFileReader<ImageType> FileReaderType;
   typedef itk::ImageFileWriter<ImageType> FileWriterType;
 
@@ -39,6 +52,41 @@ void DoAdditiveGaussianNoiseImageFilter(
 
   ImageType::Pointer outputImage = 
     additiveGaussianNoiseImageFilter->GetOutput();
+
+  SquaredDifferenceImageFilterType::Pointer squaredDifferenceImageFilter =
+    SquaredDifferenceImageFilterType::New();
+
+  squaredDifferenceImageFilter->SetInput1(inputImage);
+  squaredDifferenceImageFilter->SetInput2(outputImage);
+  squaredDifferenceImageFilter->Update();
+
+  StatisticsImageFilterType::Pointer statisticsImageFilter =
+    StatisticsImageFilterType::New();
+
+  statisticsImageFilter->SetInput(squaredDifferenceImageFilter->GetOutput());
+  statisticsImageFilter->Update();
+
+  const ValueType meanSquareError = statisticsImageFilter->GetMean();
+
+  statisticsImageFilter->SetInput(outputImage);
+  statisticsImageFilter->Update();
+
+  const ValueType maxValue = statisticsImageFilter->GetMaximum();
+  const ValueType peakSignalToNoiseRatioDecibel = 10 * log10(maxValue * maxValue / meanSquareError);
+
+  typedef itk::MetaDataDictionary MetaDataDictionaryType;
+
+  BOOST_LOG_TRIVIAL(info) << "Mean Square Error  = " << meanSquareError;
+  BOOST_LOG_TRIVIAL(info) << "Peak Signal To Noise Ratio = " << peakSignalToNoiseRatioDecibel << " dB";
+
+  MetaDataDictionaryType outMetaData;
+
+  EncapsulateMetaData(outMetaData, "(Mean)", mean);
+  EncapsulateMetaData(outMetaData, "(StandardDeviation)", standardDeviation);
+  EncapsulateMetaData(outMetaData, "(MeanSquareError)", meanSquareError);
+  EncapsulateMetaData(outMetaData, "(PeakSignalToNoiseRatioDecibel)", peakSignalToNoiseRatioDecibel);
+
+  outputImage->SetMetaDataDictionary(outMetaData);
 
   FileWriterType::Pointer imageWriter =
     FileWriterType::New();
